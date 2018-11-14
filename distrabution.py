a1701de03aa474d724e5e8391a3bbeef15cd279c
og <og@realview.dk>
2018-11-14 08:23:34 +0100

    testing

--- distrabution.py
+++ distrabution.py
@@ -3,6 +3,41 @@ import requests
 import json
 import datetime
 
+class FibonacciRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='datdb.cphbusiness.dk', port='5672'))
+        self.channel = self.connection.channel()
+        #self.connection_2 = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        #self.channel_2 = self.connection_2.channel()
+        #self.channel_2.queue_declare(queue='hello')
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        print(str(body))
+        #if self.corr_id == props.correlation_id:
+        #self.channel_2.basic_publish(exchange='',
+        #              routing_key='hello',
+        #              body=body)
+        self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.channel.basic_publish(exchange='cphbusiness.bankJSON',
+                                   routing_key='',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
@@ -10,20 +45,23 @@ channel = connection.channel()
 channel.queue_declare(queue='distrabution')
 
 def bankXML(json_string):
-	XML_conn = pika.BlockingConnection(pika.ConnectionParameters(host='datdb.cphbusiness.dk', port='5672'))
-	XML_channel = XML_conn.channel()
-	queue = channel.queue_declare().method.queue
+	response = fibonacci_rpc.call(f"""<LoanRequest>
+							<ssn>{ssn}</ssn>
+							<creditScore>{cs}</creditScore>
+							<loanAmount>{loan_amount}</loanAmount>
+							<loanDuration>{duration}</loanDuration>
+							</LoanRequest>""")
+	print(" [.] Got %r" % response)
 
-	def on_response(ch, method, props, body):
-		print(" [x] Received %r" % body)
+	
 
 	ssn = json_string["ssn"]
 	cs = json_string["credit_score"]
 	loan_amount = json_string["loan_amount"]
 	duration = datetime.datetime.now() + datetime.timedelta(days=json_string["loan_duration"])
 	channel.basic_publish(exchange='cphbusiness.bankXML',
-                      routing_key='',
-                      body=f"""<LoanRequest>
+						routing_key='',
+						body=f"""<LoanRequest>
 							<ssn>{ssn}</ssn>
 							<creditScore>{cs}</creditScore>
 							<loanAmount>{loan_amount}</loanAmount>
