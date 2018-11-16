239cfc10b951c208ac39d0a4a9308546f78e5095
Michael <2750daugbjerg@gmail.com>
2018-11-16 17:25:13 +0100

    made the banks and the loanhandler testable and created fully functional tests for pluto_bank.py

--- saturn_bank.py
+++ saturn_bank.py
@@ -21,23 +21,24 @@ body = [
 @app.route('/request', methods=['POST'])
 def get_request():
 	json_string = request.get_json()
+	data = handle_request(json_string)
+	return json.dumps(data)
+
+def handle_request(json_string):
 	ssn = json_string['ssn']
 	interest_rate = calc_interest_rate(json_string)
 	data = {'ssn': ssn, 'interest_rate': interest_rate}
-	return json.dumps(data)
-
+	return data
 
 def calc_interest_rate(body):
 	credit_score = body['creditScore']
 	loan_amount = body['loanAmount']
 	loan_duration = body['loanDuration']
-	print(body)
 	if loan_amount > 1000000:
 		interest_rate = 1
 	else:
 		interest_rate = 3
 	return interest_rate
-
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=5000)
