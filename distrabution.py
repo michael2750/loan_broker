a7e1b240ca855af7a238d0f3b5204af95fe2704b
og <og@realview.dk>
2018-11-18 17:44:09 +0100

    added case for plutobank

--- distrabution.py
+++ distrabution.py
@@ -38,6 +38,40 @@ class FibonacciRpcClient(object):
         return self.response
 
 
+class PlutoBank(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        #self.connection_2 = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        #self.channel_2 = self.connection_2.channel()
+        #self.channel_2.queue_declare(queue='hello')
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        print(str(body))
+        #if self.corr_id == props.correlation_id:
+        #self.channel_2.basic_publish(exchange='',
+        #              routing_key='hello',
+        #              body=body)
+        self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.channel.basic_publish(exchange='',
+                                   routing_key='loan_request',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
@@ -60,6 +94,11 @@ def bankXML(json_string):
 
 # 1973-01-01 01:00:00.0 CET
 
+def pluto_bank(json_string):
+    pluto_bank = PlutoBank()
+    response = pluto_bank.call(json_string)
+    print(" PLUTOBANK [.] Got %r" % response)
+
 def callback(ch, method, properties, body):
 	json_string = json.loads(body)
 	print(json.dumps(json_string))
@@ -67,7 +106,7 @@ def callback(ch, method, properties, body):
 		if bank == "Amagerbanken":
 			pass
 		if bank == "Nordea":
-			pass
+			pluto_bank(json_string)
 		if bank == "Banknordic":
 			bankXML(json_string)
 		if bank == "Danskebank":
