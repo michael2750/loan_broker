264feff6765ba9195abea19b5183e1ce7b8d2720
og <og@realview.dk>
2018-11-18 21:24:01 +0100

    testing

--- distrabution.py
+++ distrabution.py
@@ -3,7 +3,7 @@ import requests
 import json
 import datetime
 
-class FibonacciRpcClient(object):
+class BankXML(object):
     def __init__(self):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='datdb.cphbusiness.dk', port='5672'))
         self.channel = self.connection.channel()
@@ -18,7 +18,6 @@ class FibonacciRpcClient(object):
                                    queue=self.callback_queue)
 
     def on_response(self, ch, method, props, body):
-        print(str(body))
         #if self.corr_id == props.correlation_id:
         #self.channel_2.basic_publish(exchange='',
         #              routing_key='hello',
@@ -33,6 +32,40 @@ class FibonacciRpcClient(object):
                                          reply_to = self.callback_queue
                                          ),
                                    body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
+class bankJSON(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='datdb.cphbusiness.dk', port='5672'))
+        self.channel = self.connection.channel()
+        #self.connection_2 = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        #self.channel_2 = self.connection_2.channel()
+        #self.channel_2.queue_declare(queue='hello')
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        #if self.corr_id == props.correlation_id:
+        #self.channel_2.basic_publish(exchange='',
+        #              routing_key='hello',
+        #              body=body)
+        self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.channel.basic_publish(exchange='cphbusiness.bankJSON',
+                                   routing_key='',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue
+                                         ),
+                                   body=json.dumps(n))
         while self.response is None:
             self.connection.process_data_events()
         return self.response
@@ -53,7 +86,6 @@ class PlutoBank(object):
                                    queue=self.callback_queue)
 
     def on_response(self, ch, method, props, body):
-        print(str(body))
         #if self.corr_id == props.correlation_id:
         #self.channel_2.basic_publish(exchange='',
         #              routing_key='hello',
@@ -83,14 +115,14 @@ def bankXML(json_string):
 	cs = json_string["credit_score"]
 	loan_amount = json_string["loan_amount"]
 	duration = (datetime.datetime.now() + datetime.timedelta(days=json_string["loan_duration"])).strftime("%Y-%m-%d %H:%M:%S")
-	fibonacci_rpc = FibonacciRpcClient()
+	fibonacci_rpc = BankXML()
 	response = fibonacci_rpc.call(f"""<LoanRequest>
 							<ssn>{ssn}</ssn>
 							<creditScore>{cs}</creditScore>
 							<loanAmount>{loan_amount}</loanAmount>
 							<loanDuration>{duration}.0 CET</loanDuration>
 							</LoanRequest>""")
-	print(" [.] Got %r" % response)
+	print(" BANKXML [.] Got %r" % response)
 
 # 1973-01-01 01:00:00.0 CET
 
@@ -98,6 +130,9 @@ def pluto_bank(json_string):
     pluto_bank = PlutoBank()
     response = pluto_bank.call(json_string)
     print(" PLUTOBANK [.] Got %r" % response)
+
+def bankJSON(json_string):
+
 
 def callback(ch, method, properties, body):
 	json_string = json.loads(body)
@@ -110,7 +145,7 @@ def callback(ch, method, properties, body):
 		if bank == "Banknordic":
 			bankXML(json_string)
 		if bank == "Danskebank":
-			pass
+			bankJSON(json_string)
 	
 
 channel.basic_consume(callback,
